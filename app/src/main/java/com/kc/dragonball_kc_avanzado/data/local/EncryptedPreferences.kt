f0e6232f6d281b5a02d279f99cac829deb9bb3eb
Sergio Amo <sergio.amo@outlook.com>
2024-02-06 13:43:29 +0100

    Move EncryptedPreferences dependencies to di LocalModule

--- app/src/main/java/com/kc/dragonball_kc_avanzado/data/local/EncryptedPreferences.kt
+++ app/src/main/java/com/kc/dragonball_kc_avanzado/data/local/EncryptedPreferences.kt
@@ -1,31 +1,16 @@
 package com.kc.dragonball_kc_avanzado.data.local
 
-import android.content.Context
-import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKey
-import dagger.hilt.android.qualifiers.ActivityContext
-import dagger.hilt.android.qualifiers.ApplicationContext
+import android.content.SharedPreferences
 import javax.inject.Inject
 import javax.inject.Singleton
 
 
 @Singleton
-class EncryptedPreferences @Inject constructor(@ApplicationContext context: Context) {
+class EncryptedPreferences @Inject constructor(private val sharedPreferences: SharedPreferences) {
 
     companion object {
         const val LOGIN_TOKEN_VALUE = "LOGIN_TOKEN_VALUE"
     }
-
-    private val masterKey =
-        MasterKey.Builder(context).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build()
-
-    private val sharedPreferences = EncryptedSharedPreferences.create(
-        context,
-        "secret_shared_prefs",
-        masterKey,
-        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-    )
 
     fun saveToken(token: String) {
         with(sharedPreferences.edit()) {
---
+++ app/src/main/java/com/kc/dragonball_kc_avanzado/di/LocalModule.kt
@@ -0,0 +1,28 @@
+package com.kc.dragonball_kc_avanzado.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class LocalModule {
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext context: Context) : SharedPreferences {
+        val masterKey =
+            MasterKey.Builder(context).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build()
+        return EncryptedSharedPreferences.create(
+            context,
+            "dragon_ball_secret_preferences",
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM,
+        )
+    }
+}
