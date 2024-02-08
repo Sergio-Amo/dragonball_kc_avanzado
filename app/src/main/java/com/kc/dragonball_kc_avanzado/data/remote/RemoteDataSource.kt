f0627545c1f4ab5cccebe95212a9bded85c9f8e9
Sergio Amo <sergio.amo@outlook.com>
2024-02-08 09:20:17 +0100

    Test error handling, not the correct method, just a quick test, I'll centralize errors for all requests at some point.

--- app/src/main/java/com/kc/dragonball_kc_avanzado/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/kc/dragonball_kc_avanzado/data/remote/RemoteDataSource.kt
@@ -1,5 +1,9 @@
 package com.kc.dragonball_kc_avanzado.data.remote
 
+import android.content.Context
+import android.net.http.NetworkException
+import android.util.Log
+import android.widget.Toast
 import com.kc.dragonball_kc_avanzado.data.remote.request.FavoriteRequest
 import com.kc.dragonball_kc_avanzado.data.remote.request.HeroRequest
 import com.kc.dragonball_kc_avanzado.data.remote.request.LocationsRequest
@@ -7,12 +11,30 @@ import com.kc.dragonball_kc_avanzado.domain.model.HeroDetail
 import com.kc.dragonball_kc_avanzado.domain.model.HeroLocation
 import com.kc.dragonball_kc_avanzado.domain.model.HeroLocationsRemote
 import com.kc.dragonball_kc_avanzado.domain.model.HeroRemote
+import dagger.hilt.android.qualifiers.ApplicationContext
 import okhttp3.Credentials
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
+import kotlin.coroutines.coroutineContext
 
-class RemoteDataSource @Inject constructor(private val api: DragonBallApi) {
-    suspend fun login(email: String, password: String): String =
-        api.login(Credentials.basic(email, password))
+class RemoteDataSource @Inject constructor(private val api: DragonBallApi, @ApplicationContext private val context: Context) {
+    suspend fun login(email: String, password: String): String {
+        // I know I shouldn't be catching errors here, but wanted to try real quick
+        // At some point I'll centralize all network errors in the same place, but I'm not
+        // sure how, yet.
+        try {
+            return api.login(Credentials.basic(email, password))
+        } catch (e: HttpException) {
+            Log.d("WTF", e.localizedMessage as String)
+            Toast.makeText(context, e.localizedMessage,Toast.LENGTH_LONG).show()
+        } catch (e: IOException) {
+            Log.d("WTF", e.toString())
+        } catch (e: Exception) {
+            Log.d("WTF", e.toString())
+        }
+        return ""
+    }
 
     suspend fun getHeroes(token: String): List<HeroRemote> = api.getHeroes(HeroRequest(), token)
 
